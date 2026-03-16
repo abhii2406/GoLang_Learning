9c16fbf5816a7769b26002b17b3f99b75ae3bc49
Abhijit Patil <abipatilagp2406@gmail.com>
2026-03-16 17:14:05 +0530

    Type Switches|Extracting Type info from values

--- Interfaces/Any_Value_Allowed_Type/main.go
+++ Interfaces/Any_Value_Allowed_Type/main.go
@@ -10,6 +10,32 @@ func main() {
 	AnyTypeValue(1.56)
 }
 
+// func AnyTypeValue(value interface{}) {
+// 	// fmt.Println(value)
+// 	switch value.(type) {
+// 	case int:
+// 		fmt.Println("Int : ", value)
+// 	case float64:
+// 		fmt.Println("Float64 : ", value)
+// 	case string:
+// 		fmt.Println("String : ", value)
+// 	}
+// }
+
 func AnyTypeValue(value interface{}) {
-	fmt.Println(value)
+	intVal, ok := value.(int)
+	if ok {
+		fmt.Println("Integer : ", intVal)
+		return
+	}
+	floatVal, ok := value.(float64)
+	if ok {
+		fmt.Println("Float : ", floatVal)
+		return
+	}
+	stringVal, ok := value.(string)
+	if ok {
+		fmt.Println("String : ", stringVal)
+		return
+	}
 }
