9ef15c190d3de5c3a72026f9b7515aa97c4b6e14
Abhijit Patil <abhii.p.2406@gmail.com>
2026-03-18 12:40:49 +0530

    for loop with array|map

---
+++ ForLoopWithArraysAndMap/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type intMap map[int]string
+
+func main() {
+	names := []string{}
+	names = append(names, "Abhi")
+	names = append(names, "Patil")
+
+	for i, val := range names {
+		fmt.Println(i, " ", val)
+	}
+
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(i, " ", names[i])
+	// }
+
+	// for _, val := range names {
+	// 	fmt.Println(val)
+	// }
+
+	m := make(intMap, 2)
+
+	m[0] = "Abhi"
+	// m[2] = "Ab"
+
+	for key, val := range m {
+		fmt.Println(key, "Map", val)
+	}
+	fmt.Println(len(m))
+
+}
